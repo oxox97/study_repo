4cfcc79cde6457c2a2a503d0e0a4e0db709d1df2
오병찬 <oxox97@naver.com>
2025-03-30 18:56:45 +0900

    upload code files

---
+++ doit algorithm codingtest/036. greedy_parentheses.py
@@ -0,0 +1,17 @@
+import re
+
+# questions = [
+#     "55-50+40",  # -35
+#     "10+20+30+40",  # 100
+#     "00009-00009",  # 0
+#     "30-40-50+20-10"  # -90 
+# ]
+
+
+q = input()
+
+plus = sum(list(map(int, q.split('-')[0].split('+'))))
+minus = sum([sum(list(map(int, i.split('+')))) if '+' in i else int(i) for i in q.split('-')[1:]])
+
+a = plus - minus
+print(a)
