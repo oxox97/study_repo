6c407ca161e860a15d7216c910b2b4d6fb305f9b
ochan <ochan@ochans-MacBook-Pro.local>
2025-04-20 14:06:35 +0900

    upload code files

--- doit algorithm codingtest/004. sum_gugan_2.py
+++ doit algorithm codingtest/004. sum_gugan_2.py
@@ -1,5 +1,5 @@
 # 2차원 구간 합
-# prefix_sum -> psum
+# 누적합 (prefix_sum -> psum) 활용
 
 import sys
 input = sys.stdin.readline
@@ -10,17 +10,18 @@ mat = [list(map(int, input().split())) for _ in range(N)]  # 0-based mat
 
 psum_mat = [[0] * (N+1) for _ in range(N+1)]  # 1-based mat
 
-# 누적합 계산
+# ⭐️ 2차원 누적합 만들기 (prefix_sum)
 for y in range(N):
     for x in range(N):
         psum_mat[y+1][x+1] = (
             psum_mat[y+1][x] + psum_mat[y][x+1]
             - psum_mat[y][x] + mat[y][x]
         ) # psum_mat은 1-based index라서 [r+1][c+1]이 아닌, [r][c]
-        
+
+# ⭐️ 구간합 계산 
 answer = []
 for _ in range(M):
-    x1, y1, x2, y2 = map(int, input().split())  # [y][x] ...? 왜지?
+    x1, y1, x2, y2 = map(int, input().split())
     a = psum_mat[x2][y2] - psum_mat[x2][y1-1] - psum_mat[x1-1][y2] + psum_mat[x1-1][y1-1]
     answer.append(a)
 
