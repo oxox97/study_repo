a29225920672748845e27541ff40dd2476d97076
ochan <ochan@ochans-MacBook-Pro.local>
2025-04-26 17:17:54 +0900

    upload code files

--- doit algorithm codingtest/008. good_number.py
+++ doit algorithm codingtest/008. good_number.py
@@ -1,34 +1,30 @@
-# 투 포인터 감 잡음
-# 자기 제외한 두 수인 조건 망각
-# 007.주몽과 같이 정렬한 리스트에서 끝에서 시작하여 작으면 s+=1, 크면 e-=1 하는 O(nlogn) * N 문제
+# 투 포인터
+# 양 끝단에서 시작 + 자기 자신 제외
+# O(nlogn) * N 문제
 
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst = sorted([int(i) for i in input().split()])
+lst = sorted(list(map(int, input().split())))
 
-answer_list = []
-
-for i, value in enumerate(lst):
-    #print("\n> value :", value)
-    s = 0
-    e = N-1
+a_list = []
+for i, v in enumerate(lst):
+    s, e = 0, len(lst)-1
     while s < e:
         if s == i:
             s += 1
-        if e == i:
+        elif e == i:
             e -= 1
-        if s != e:
-            current_sum = lst[s] + lst[e]
-            #print(lst[s], '+', lst[e], '=', current_sum)
-            if current_sum == value:
-                answer_list.append([lst[s], lst[e], value])
+        else:
+            curr_sum = lst[s] + lst[e]
+            # print(v, curr_sum)
+            if curr_sum == v:
+                a_list.append(v)
                 break
-            elif current_sum > value:
-                e -= 1
-            else:
+            elif curr_sum < v:
                 s += 1
+            else:
+                e -= 1
 
-#print(answer_list)
-print(len(answer_list))
+print(len(a_list))
