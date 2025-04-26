bd9090a68917703b87197daf3ede39fda0435caf
ochan <ochan@ochans-MacBook-Pro.local>
2025-04-26 16:33:19 +0900

    upload code files

--- doit algorithm codingtest/007. jumong.py
+++ doit algorithm codingtest/007. jumong.py
@@ -1,28 +1,29 @@
-# 다시 풀기
-# 양 끝단에서 시작하는 거 간단한데 생각 못 했다..
-# 중간에서 시작하면 일부 조합 못 찾음
-# 투 포인터는 반드시 끝 단에서 시작!!!
+# 투포인터
+# sorted 양 끝단에서 줄이기
+# 합이 기준보다 크면, e -= 1 / 작으면, s += 1
+
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-
 lst = sorted([int(i) for i in input().split()])
 
+# print(lst)
+
 s = 0
-e = N-1
-current_sum = 0
-answer_list = []
+e = len(lst)-1
+cnt = 0
 
 while s < e:
-    current_sum = lst[s] + lst[e]
-    if current_sum == M:
-        answer_list.append([s,e])
+    curr_sum = lst[s] + lst[e]
+    if curr_sum == M:
+        cnt += 1
         s += 1
         e -= 1
-    elif current_sum < M:
-        s += 1
+    elif curr_sum > M:
+        e -= 1
     else:
-        e -= 1
+        s += 1
 
-
-print(len(answer_list))
+print(cnt)
