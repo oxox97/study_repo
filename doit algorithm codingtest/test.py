ccd2c943594c1e906edd7aac85d9ab7926cee784
ochan <ochan@ochans-MacBook-Pro.local>
2025-04-26 16:33:19 +0900

    upload code files

--- doit algorithm codingtest/test.py
+++ doit algorithm codingtest/test.py
@@ -1,21 +1,26 @@
-# 오큰수 (NGE)
-
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst = list(map(int, input().split()))
+M = int(input())
+lst = sorted([int(i) for i in input().split()])
 
-answer = [-1] * (N)
-stk = []  # ⭐️ 오큰수가 정해지지 않은 리스트의 인덱스 저장
+# print(lst)
 
-for i in range(N):
-    # 오큰수가 정해지지 않은 인덱스가 있고 (len(stk) > 0) &
-    # 현재 기준 값이 스택 top 인덱스의 값보다 크면 (lst[i] > lst[stk[-1]])
-    # 스택에서 해당 인덱스 제거(pop)하면서 오큰수 할당
-    while stk and lst[i] > lst[stk[-1]]:
-        answer[stk.pop()] = lst[i]   
-    stk.append(i)  # 차례대로 인덱스 넣어주기
+s = 0
+e = len(lst)-1
+cnt = 0
 
-for a in answer:
-    print(a, end=" ")
+while s < e:
+    curr_sum = lst[s] + lst[e]
+    if curr_sum == M:
+        cnt += 1
+        s += 1
+        e -= 1
+    elif curr_sum > M:
+        e -= 1
+    else:
+        s += 1
+
+print(cnt)
+        
