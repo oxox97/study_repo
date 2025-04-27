9b52029df00174b56f1812fa9c496e1792d5faba
ochan <ochan@ochans-MacBook-Pro.local>
2025-04-27 21:05:00 +0900

    upload code files

---
+++ CS/api/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Dict
+
+app = FastAPI()
+
+# 가격표
+price_table = {
+    "apple": 3.5,
+    "banana": 2.0,
+    "orange": 4.0
+}
+
+class Item(BaseModel):
+    name: str
+
+@app.post("/get-price/")
+def get_price(item: Item):
+    price = price_table.get(item.name.lower())
+    if price is not None:
+        return {"name": item.name, "price": price}
+    else:
+        return {"error": "Item not found"}
+    
+@app.get("/items/", response_model=Dict[str, float])
+def get_all_items():
+    return price_table
